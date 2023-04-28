188237f1d5daba1c853c204edfc8460d15e1a6fa
Фируз Разаков <razackof@gmail.com>
2023-04-28 11:06:05 +0500

    Мигрировать БД

---
+++ Data/Migrations/20230428060402_addedCreatorAndStatusToInvoice.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IdentityApp.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedCreatorAndStatusToInvoice : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CreatorId",
+                table: "Invoice",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Invoice",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatorId",
+                table: "Invoice");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Invoice");
+        }
+    }
+}
