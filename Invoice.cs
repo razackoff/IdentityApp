27615242e762fd0f4074d67c01b167b890164c54
razac <razac@RAZAKOF>
2023-04-28 10:33:01 +0500

    Добавьте файлы проекта.

---
+++ Invoice.cs
@@ -0,0 +1,10 @@
+﻿namespace IdentityApp
+{
+    public class Invoice
+    {
+        public int InvoiceId { get; set; }
+        public decimal InvoiceAmount { get; set; }
+        public string InvoiceMonth { get; set; }
+        public string InvoiceOwner { get; set;}
+    }
+}
