157ac9a8bd27f7f76ff2f8eea65599e15d83d59b
Фируз Разаков <razackof@gmail.com>
2023-04-28 11:05:37 +0500

    Добавить поле CreatorId

---
+++ Models/Invoice.cs
@@ -0,0 +1,22 @@
+﻿namespace IdentityApp.Models
+{
+    public class Invoice
+    {
+        public int InvoiceId { get; set; }
+        public decimal InvoiceAmount { get; set; }
+        public string InvoiceMonth { get; set; }
+        public string InvoiceOwner { get; set; }
+        public string CreatorId { get; set; }
+        public InvoiceStatus Status { get; set; }
+    }
+}
+
+namespace IdentityApp
+{
+    public enum InvoiceStatus
+    {
+        Submitted,
+        Approved,
+        Rejected
+      }
+}
