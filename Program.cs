6876d311ecbedd735d9354ad1d614d222dd4ae07
Фируз Разаков <razackof@gmail.com>
2023-04-30 22:06:53 +0500

    .

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
